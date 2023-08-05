1b68f2a209a7fc4bccf0ff87501efe2398df5c35
your name goes here <_vodka_alla_cioccolata_@live.it>
2023-08-05 14:48:55 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
       template: "./src/index.html",
     }),
     new Dotenv({
-      path: path.resolve(__dirname, ".env")
+      path: path.resolve(__dirname, ".env"),
     }),
   ],
 
